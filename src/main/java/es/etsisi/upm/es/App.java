eb73cfcb67bf61ff84f7ca1f28f0ec6366086aed
Raul Gonzalo Toapanta <gonzalo.tpaucar@alumnos.upm.es>
2025-04-04 14:45:25 +0200

    Inicializar

---
+++ src/main/java/es/etsisi/upm/es/App.java
@@ -0,0 +1,13 @@
+package es.etsisi.upm.es;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
