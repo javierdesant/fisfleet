e7ad7c6920adb43382226c7426001c46dc3ee2bc
Javier de Santiago <j.desantiagosoto@gmail.com>
2025-04-30 13:08:26 +0200

    feat: add initial Maven-based Spring Boot project setup
    This commit initializes a Spring Boot project with Maven. It includes essential files such as `pom.xml`, Spring Boot application classes, configuration files, test setup, and Maven wrapper scripts. Dependencies for JPA, Security, and PostgreSQL are added along with support for a Battleship game module.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/FisfleetApplication.java
@@ -0,0 +1,15 @@
+package es.upm.etsisi.fis.fisfleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisfleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisfleetApplication.class, args);
+
+
+	}
+
+}
---
+++ src/test/java/es/upm/etsisi/fis/fisfleet/FisfleetApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.etsisi.fis.fisfleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisfleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
