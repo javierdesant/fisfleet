2667d1a4e642ec40db55e9f06018e9435a5cfaad
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-12 15:07:54 +0200

    feat(dto): add GameRequest and GameResponse classes
    Introduced GameRequest and GameResponse classes for API data transfer. GameRequest includes validation annotations, while GameResponse handles game-related data, including player IDs, dates, winner, and moves.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/api/dto/requests/GameRequest.java
@@ -0,0 +1,34 @@
+package es.upm.etsisi.fis.fisfleet.api.dto.requests;
+
+import es.upm.etsisi.fis.fisfleet.api.validation.ValidWinnerId;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@ValidWinnerId
+public class GameRequest {
+
+    @NotNull(message = "Player1Id cannot be empty.")
+    private Long player1Id;
+
+    @NotNull(message = "Player2Id cannot be empty.")
+    private Long player2Id;
+
+    // FIXME maybe it can be generated on the spot? idk
+    @NotNull(message = "The start date cannot be empty.")
+    private Instant startDate;
+
+    @NotNull(message = "The end date cannot be empty.")
+    private Instant endDate;
+
+    @NotNull(message = "The winnerId cannot be empty.")
+    private Long winnerId;
+}
---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/api/dto/responses/GameResponse.java
@@ -0,0 +1,23 @@
+package es.upm.etsisi.fis.fisfleet.api.dto.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class GameResponse {
+    private Long id;
+    private Long player1Id;
+    private Long player2Id;
+    private Instant startDate;
+    private Instant endDate;
+    private Long winnerId;
+    private List<MoveResponse> moves;
+}
