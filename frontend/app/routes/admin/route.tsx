ac85d251e282d0f27b3803f27390236d41a2099d
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-15 19:17:21 +0200

    feat: update admin and landing page layouts
    Refactored admin and landing pages with updated designs and components. Improved styling, renamed layout components, and enhanced semantics for better readability and functionality.

--- frontend/app/routes/admin/route.tsx
+++ frontend/app/routes/admin/route.tsx
@@ -4,10 +4,15 @@ import React from "react";
 export const meta: Route.MetaFunction = () => {
   return [
     { title: "Admin" },
-    { name: "description", content: "Admin panel" },
+    { name: "description", content: "Admin dashboard" },
   ];
 };
 
-export default function adminPage() {
-  return <h1>admin</h1>;
+export default function AdminPage() {
+  return (
+    <div className="p-4 bg-onyx-500 dark:bg-cadet_gray-500 rounded-lg text-seasalt-500">
+      <h1 className="text-2xl mb-3">Admin Dashboard</h1>
+      <p>Manage matches and users here.</p>
+    </div>
+  );
 }
