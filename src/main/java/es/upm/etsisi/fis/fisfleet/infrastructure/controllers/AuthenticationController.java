78b6a15786fd2d656f2e78613d212cfc7d0ec859
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-15 20:18:04 +0200

    feat(auth): add AuthenticationController and prepare LDAPAuthService
    Introduced `AuthenticationController` with a basic structure and endpoints. Enhanced `LDAPAuthServiceImpl` with placeholders for authentication logic and added dependencies for authentication management.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/AuthenticationController.java
@@ -0,0 +1,13 @@
+package es.upm.etsisi.fis.fisfleet.infrastructure.controllers;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthenticationController {
+    // TODO
+}
--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/LDAPAuthServiceImpl.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/LDAPAuthServiceImpl.java
@@ -5,11 +5,21 @@ import es.upm.etsisi.fis.fisfleet.api.dto.requests.UserRequest;
 import es.upm.etsisi.fis.fisfleet.api.dto.responses.AuthenticationResponse;
 import es.upm.etsisi.fis.fisfleet.api.dto.responses.UserResponse;
 import es.upm.etsisi.fis.fisfleet.domain.entities.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 import servidor.Autenticacion;
 
-// Should thgis be called LDAPAuthServiceImpl or just LDAPAuthService?
+// Should this be called LDAPAuthServiceImpl or just LDAPAuthService?
+@Service
+@Slf4j
+@AllArgsConstructor
 public class LDAPAuthServiceImpl implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+
 
     @Override
     public UserResponse register(UserRequest request) {
@@ -19,6 +29,25 @@ public class LDAPAuthServiceImpl implements AuthenticationService {
 
     @Override
     public AuthenticationResponse login(AuthenticationRequest request) {
+
+
+//          TODO: implement basic auth service like...
+//                Authentication authentication = new UsernamePasswordAuthenticationToken(
+//                        request.getUsername(), request.getPassword()
+//                );
+//                authenticationManager.authenticate(authentication);
+//                UserEntity user = userRepository.findByEmail(request.getUsername())
+//                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+//                String jwt = tokenService.generateToken(user, buildExtraClaims(user));
+//                return new AuthenticationResponse(jwt);
+//        Do i have to do it for UsernamePasswordAuthenticationToken? Maybe its a type of OAuth
+//        TODO: create extra claims fn
+//        are there extra claim in OAuth???
+
+
+
+
+
         String email = "email"; // TEST
 //                request.getEmail(); FIXME
         String password = request.getPassword();
