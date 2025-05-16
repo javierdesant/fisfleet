76b37b7d8ca149cd015a98aa95efcae2c81f7ccb
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 20:45:31 +0200

    feat(repository): add findByUsernameHash method
    Added a method to find a UserEntity by its username hash in the UserRepository. This enables more specific user lookups.

--- src/main/java/es/upm/etsisi/fis/fisfleet/domain/repositories/UserRepository.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/domain/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package es.upm.etsisi.fis.fisfleet.domain.repositories;
 import es.upm.etsisi.fis.fisfleet.domain.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsernameHash(String usernameHash);
 }
