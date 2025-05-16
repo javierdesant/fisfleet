af6268ea09661df16fdf5e41d34b407c6b12d9ce
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 20:44:47 +0200

    feat: add web-based game control and new REST endpoint
    Integrates web-based controller using WebSocket for interactions and caching for requests. Adds MatchController with initial REST API to handle game creation.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/MatchController.java
@@ -0,0 +1,28 @@
+package es.upm.etsisi.fis.fisfleet.infrastructure.controllers;
+
+import es.upm.etsisi.fis.controller.ControladorPartida;
+import es.upm.etsisi.fis.fisfleet.infrastructure.adapters.VistaWebControladoraPartida;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/partida")
+public class MatchController {
+
+    private final ControladorPartida controladorPartida;
+
+    @Autowired
+    public MatchController(VistaWebControladoraPartida vistaWeb) {
+        this.controladorPartida = ControladorPartida.getInstance(null);
+        this.controladorPartida.setGui(vistaWeb);
+    }
+
+    @PostMapping("/crear")  // FIXME: change me pls
+    public ResponseEntity<Void> crearPartida() {
+        return null; // TODO
+        //  remove <Void>?
+    }
+}
