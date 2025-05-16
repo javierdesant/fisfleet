c7f4c9974d8854353557748722808496e0a1f29f
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 02:31:07 +0200

    feat: add hover effects for Cell component
    Enhanced the Cell component with hover transitions for smoother visual feedback. Included group-hover effects to improve user interaction and pointer cursor for better usability.

--- frontend/app/routes/match_.$id._index/Cell.tsx
+++ frontend/app/routes/match_.$id._index/Cell.tsx
@@ -41,13 +41,12 @@ const Cell: React.FC<CellProps> = ({
         : "bg-gray-200";
 
   return (
-    <div className="relative aspect-square h-full w-full">
-      {" "}
+    <div className="group relative aspect-square h-full w-full">
       <div
-        className={`absolute inset-0 translate-x-[2px] translate-y-[2px] rounded-md bg-black md:translate-x-[3px] md:translate-y-[3px]`}
+        className={`absolute inset-0 translate-x-[2px] translate-y-[2px] rounded-md bg-black transition-all duration-200 ease-in-out group-hover:translate-x-0 group-hover:translate-y-0 group-hover:opacity-0 md:translate-x-[3px] md:translate-y-[3px]`}
       />
       <div
-        className={`absolute inset-0 flex items-center justify-center ${bg} rounded-md border-2 border-black sm:border-3 md:border-3`}
+        className={`absolute inset-0 flex cursor-pointer items-center justify-center ${bg} rounded-md border-2 border-black transition-all duration-200 ease-in-out group-hover:translate-x-[2px] group-hover:translate-y-[2px] sm:border-3 md:border-3 md:group-hover:translate-x-[3px] md:group-hover:translate-y-[3px]`}
       >
         {renderContent()}
       </div>
