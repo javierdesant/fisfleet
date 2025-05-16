ff38261bf28163cf1aa59d575336aecd8d77d5ee
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 02:17:39 +0200

    feat: update match layout styling
    Revised the match layout component to use a gradient background and updated text colors for better visual contrast. Simplifies styling and improves dark mode support.

--- frontend/app/routes/match_.$id/index.tsx
+++ frontend/app/routes/match_.$id/index.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export default function MatchLayout() {
   return (
-    <div className="bg-marian_blue-500 dark:bg-marian_blue-500 p-4 rounded-lg shadow-md">
-      <Outlet />
-    </div>
+      <div className="flex grow bg-gradient-to-br from-alice_blue-500 to-alice_blue-600 dark:from-oxford_blue-500 dark:to-oxford_blue-400 text-eerie_black-500 dark:text-seasalt-500">
+        <Outlet />
+      </div>
   );
 }
