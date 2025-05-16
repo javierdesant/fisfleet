d3fce83c3087c6456f7252afb2e6c74da322fa48
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 02:26:31 +0200

    fix: adjust MatchLayout component
    Reorders CSS classes in the MatchLayout component and add flex-col for consistent styling. Ensures proper application of styles across different themes.

--- frontend/app/routes/match_.$id/index.tsx
+++ frontend/app/routes/match_.$id/index.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router";
 
 export default function MatchLayout() {
   return (
-      <div className="flex grow bg-gradient-to-br from-alice_blue-500 to-alice_blue-600 dark:from-oxford_blue-500 dark:to-oxford_blue-400 text-eerie_black-500 dark:text-seasalt-500">
-        <Outlet />
-      </div>
+    <div className="from-alice_blue-500 to-alice_blue-600 dark:from-oxford_blue-500 dark:to-oxford_blue-400 text-eerie_black-500 dark:text-seasalt-500 flex grow flex-col bg-gradient-to-br">
+      <Outlet />
+    </div>
   );
 }
