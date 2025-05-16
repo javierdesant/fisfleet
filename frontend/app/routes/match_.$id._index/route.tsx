b9ac5de6014cf5c8bed2db94b2777e4562779c34
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 02:17:22 +0200

    feat: update BattlePage layout and styles
    Refactored the BattlePage to use a flexible column-to-row layout for better responsiveness. Updated styles for improved visual hierarchy and alignment.

--- frontend/app/routes/match_.$id._index/route.tsx
+++ frontend/app/routes/match_.$id._index/route.tsx
@@ -12,13 +12,20 @@ export const meta: Route.MetaFunction = () => {
 
 export default function BattlePage() {
   return (
-    <div className="grid grid-cols-2 gap-4">
-      <div className="border border-onyx-500 dark:border-white-500 p-2 rounded-lg bg-alice_blue-500 dark:bg-oxford_blue-500">
-        <h2 className="text-xl font-semibold mb-2 text-eerie_black-500 dark:text-seasalt-500">Enemy Fleet</h2>
+    <div className="flex flex-col lg:flex-row gap-4">
+      <div className="p-2 m-2 grow">
+        <h2 className="text-eerie_black-500 dark:text-seasalt-500 mb-4 font-bold text-2xl md::text-3xl text-center bg-alice_blue-200">
+          Your Fleet
+        </h2>
         <EnemyBoard />
       </div>
-      <div className="border border-onyx-500 dark:border-white-500 p-2 rounded-lg bg-alice_blue-500 dark:bg-oxford_blue-500">
-        <h2 className="text-xl font-semibold mb-2 text-eerie_black-500 dark:text-seasalt-500">Your Fleet</h2>
+
+      <div className="border-onyx-800 dark:border-white-500 min-h-full border"/>
+
+      <div className="p-2 m-2 grow">
+        <h2 className="text-eerie_black-500 dark:text-seasalt-500 mb-4 font-bold text-2xl md::text-3xl text-center bg-alice_blue-200">
+          Enemy Fleet
+        </h2>
         <PlayerBoard />
       </div>
     </div>
