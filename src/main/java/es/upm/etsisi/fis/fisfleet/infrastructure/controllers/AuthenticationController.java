64ae3b966354f9441ef0076309429237eafcdb56
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-16 20:45:13 +0200

    chore: move annotation to correct position
    Moved the @AllArgsConstructor annotation above the class declaration instead of below @RequestMapping. This ensures proper initialization by the framework and adheres to coding conventions.

--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/AuthenticationController.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/AuthenticationController.java
@@ -5,9 +5,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/auth")
-@AllArgsConstructor
 public class AuthenticationController {
     // TODO
 }
