a8831e003e270108b8286f159855d5338802c265
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-17 21:39:08 +0200

    refactor: rename GameRepository to GameResultRepository
    Renamed the GameRepository interface to GameResultRepository for better clarity and alignment with its purpose. No functional changes were made.

--- src/main/java/es/upm/etsisi/fis/fisfleet/domain/repositories/GameResultRepository.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/domain/repositories/GameResultRepository.java
@@ -3,5 +3,5 @@ package es.upm.etsisi.fis.fisfleet.domain.repositories;
 import es.upm.etsisi.fis.fisfleet.domain.entities.GameResultEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameRepository extends JpaRepository<GameResultEntity, Long> {
+public interface GameResultRepository extends JpaRepository<GameResultEntity, Long> {
 }
