c9d2f56aa636fdbb0a90501437b0fd5f00f6e6ef
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-17 21:19:16 +0200

    feat: implement user creation logic
    Added functionality to create a new user, including authentication via LDAP and initialization of user attributes. The user entity is now saved to the repository.

--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/UserServiceImpl.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/UserServiceImpl.java
@@ -2,17 +2,44 @@ package es.upm.etsisi.fis.fisfleet.infrastructure.services;
 
 import es.upm.etsisi.fis.fisfleet.api.dto.requests.UserRequest;
 import es.upm.etsisi.fis.fisfleet.domain.entities.UserEntity;
+import es.upm.etsisi.fis.fisfleet.domain.repositories.UserRepository;
+import es.upm.etsisi.fis.fisfleet.infrastructure.config.security.LDAPAuthenticator;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import servidor.ObtencionDeRol;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Optional;
 
 @Slf4j
 @AllArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
     @Override
     public UserEntity create(UserRequest request) {
-        return null;    // FIXME
+        String username = request.getUsername();
+        log.info("Creating user with username: {}", username);
+
+        String usernameHash = LDAPAuthenticator.authenticate(username);
+
+        UserEntity newUser = UserEntity.builder()
+                .usernameHash(usernameHash)
+                .alias(request.getAlias())
+                .registrationDate(Instant.now())
+                .UPMUserType(ObtencionDeRol.get_UPM_AccountRol(username))
+                .moves(new HashSet<>())
+                .scores(new HashSet<>())
+                .gamesAsPlayer1(new HashSet<>())
+                .gamesAsPlayer2(new HashSet<>())
+                .gamesWon(new HashSet<>())
+                .build();
+
+        return userRepository.save(newUser);
     }
 
     @Override
