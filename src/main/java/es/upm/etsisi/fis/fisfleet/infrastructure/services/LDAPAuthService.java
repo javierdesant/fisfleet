17a68b70512f1ef7797317669f0e93fac5a9c744
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-17 03:23:03 +0200

    feat(auth)!: improve LDAP authentication service
    Refactored LDAPAuthService to enhance authentication functionality by implementing user registration, login, and extra claims generation. Added methods for hashing, token generation, and integrating with UserRepository and UserMapper.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/LDAPAuthService.java
@@ -0,0 +1,90 @@
+package es.upm.etsisi.fis.fisfleet.infrastructure.services;
+
+import es.upm.etsisi.fis.fisfleet.api.dto.requests.AuthenticationRequest;
+import es.upm.etsisi.fis.fisfleet.api.dto.requests.UserRequest;
+import es.upm.etsisi.fis.fisfleet.api.dto.responses.AuthenticationResponse;
+import es.upm.etsisi.fis.fisfleet.api.dto.responses.UserResponse;
+import es.upm.etsisi.fis.fisfleet.api.mappers.UserMapper;
+import es.upm.etsisi.fis.fisfleet.domain.entities.UserEntity;
+import es.upm.etsisi.fis.fisfleet.domain.repositories.UserRepository;
+import es.upm.etsisi.fis.fisfleet.utils.RoleMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import utilidades.Cifrado;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@AllArgsConstructor
+@Service
+public class LDAPAuthService implements AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final UserMapper userMapper;
+
+
+    @Override
+    public UserResponse register(UserRequest request) {
+        UserEntity user = userService.create(request);
+        return userMapper.mapToUserResponse(user);
+    }
+
+    @Override
+    public AuthenticationResponse login(AuthenticationRequest request) {
+        String usernameHash = Cifrado.cifrar(request.getUsername());
+        UserEntity user = this.findUserByUsernameHash(usernameHash);
+
+        this.authenticateUser(usernameHash, user.getPassword(), user.getAuthorities());
+
+        String generatedToken = tokenService.generateToken(user, buildExtraClaims(user));
+
+        return new AuthenticationResponse(generatedToken);
+    }
+
+    private UserEntity findUserByUsernameHash(String usernameHash) {
+        return userRepository.findByUsernameHash(usernameHash)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    private void authenticateUser(String usernameHash, String password, Collection<? extends GrantedAuthority> authorities) {
+        UsernamePasswordAuthenticationToken authToken =
+                new UsernamePasswordAuthenticationToken(usernameHash, password, authorities);
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+    }
+
+    private Map<String, Object> buildExtraClaims(UserEntity user) {
+        Map<String, Object> extraClaims = new HashMap<>();
+        extraClaims.put("usernameHash", user.getUsernameHash());
+        extraClaims.put("authorities", user.getAuthorities());
+        extraClaims.put("role", RoleMapper.getRoleForUPMUser(user.getUPMUserType()));
+        return extraClaims;
+    }
+
+
+    @Override
+    public UserEntity findLoggedInUser() {
+        UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (auth == null || auth.getPrincipal() == null) {
+            throw new IllegalStateException("No user logged in");
+        }
+
+        String usernameHash = (String) auth.getPrincipal();
+
+        return this.findUserByUsernameHash(usernameHash);
+    }
+}
