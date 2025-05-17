f2536ce6aecb175f241fec99eee62d1531834863
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-17 21:34:40 +0200

    feat: enhance LDAPAuthenticationController with new endpoints
    Added endpoints for token validation and retrieving the logged-in user's profile. These features improve authentication handling and user information accessibility.

--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/LDAPAuthenticationController.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/LDAPAuthenticationController.java
@@ -2,14 +2,15 @@ package es.upm.etsisi.fis.fisfleet.infrastructure.controllers;
 
 import es.upm.etsisi.fis.fisfleet.api.dto.requests.AuthenticationRequest;
 import es.upm.etsisi.fis.fisfleet.api.dto.responses.AuthenticationResponse;
+import es.upm.etsisi.fis.fisfleet.api.dto.responses.UserResponse;
+import es.upm.etsisi.fis.fisfleet.api.mappers.UserMapper;
+import es.upm.etsisi.fis.fisfleet.domain.entities.UserEntity;
 import es.upm.etsisi.fis.fisfleet.infrastructure.services.AuthenticationService;
+import es.upm.etsisi.fis.fisfleet.infrastructure.services.TokenService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -17,10 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class LDAPAuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final TokenService tokenService;
+    private final UserMapper userMapper;
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request) {
         AuthenticationResponse response = authenticationService.login(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<Boolean> validate(@RequestParam String jwt) {
+        boolean isValidToken = tokenService.validateToken(jwt);
+        return ResponseEntity.ok(isValidToken);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<UserResponse> findMyProfile() {
+        UserEntity user = authenticationService.findLoggedInUser();
+        UserResponse userResponse = userMapper.mapToUserResponse(user);
+        return ResponseEntity.ok(userResponse);
     }
 }
