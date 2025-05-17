fcb1f84d6e47e0e58da0ef75c0c8946c74da9c01
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-17 03:16:53 +0200

    feat: add validation for AuthenticationRequest fields
    Introduce validation annotations for `username` and `password` in `AuthenticationRequest`. Ensure these fields are non-blank and `username` adheres to UPM email format.

--- src/main/java/es/upm/etsisi/fis/fisfleet/api/dto/requests/AuthenticationRequest.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/api/dto/requests/AuthenticationRequest.java
@@ -1,5 +1,7 @@
 package es.upm.etsisi.fis.fisfleet.api.dto.requests;
 
+import es.upm.etsisi.fis.fisfleet.api.validation.ValidUPMEmail;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,10 @@ import java.io.Serializable;
 @AllArgsConstructor
 @Data
 public class AuthenticationRequest implements Serializable {
+    @NotBlank(message = "The username cannot be empty.")
+    @ValidUPMEmail
     private String username;    // email in this case
+
+    @NotBlank(message = "The password cannot be empty.")
     private String password;
 }
