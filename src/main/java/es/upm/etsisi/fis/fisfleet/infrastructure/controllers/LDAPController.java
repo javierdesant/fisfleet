3389ee30c49c166e80cb62516eb236500d7e5dd2
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-17 03:19:17 +0200

    feat(auth): add LDAP authentication endpoint
    Introduced a POST `/api/auth/authenticate` endpoint to handle LDAP user authentication. Integrated with `AuthenticationService` to process requests and return appropriate responses.

--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/LDAPController.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/LDAPController.java
@@ -1,14 +1,26 @@
 package es.upm.etsisi.fis.fisfleet.infrastructure.controllers;
 
+import es.upm.etsisi.fis.fisfleet.api.dto.requests.AuthenticationRequest;
+import es.upm.etsisi.fis.fisfleet.api.dto.responses.AuthenticationResponse;
+import es.upm.etsisi.fis.fisfleet.infrastructure.services.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/auth/ldap")
+@RequestMapping("/api/auth")
 public class LDAPController {
 
+    private final AuthenticationService authenticationService;
 
-    // TODO
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody AuthenticationRequest request) {
+        AuthenticationResponse response = authenticationService.login(request);
+        return ResponseEntity.ok(response);
+    }
 }
