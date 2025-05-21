3b7282ba5ca3ab1b8cad2dda47e942fb5bd42511
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-21 03:19:36 +0200

    feat(cache): add initial CacheConfig class
    Introduce a new CacheConfig class with basic setup for Caffeine caching. This is a placeholder implementation with a TODO for further configuration.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/cache/CacheConfig.java
@@ -0,0 +1,8 @@
+package es.upm.etsisi.fis.fisfleet.infrastructure.cache;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+    // TODO: add caffeine constructors
+}
