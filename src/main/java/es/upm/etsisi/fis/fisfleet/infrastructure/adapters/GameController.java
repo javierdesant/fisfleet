74caec58e832966baac5820003e0eed8b525cbcd
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-21 03:20:17 +0200

    feat: add GameController adapter for game logic integration
    Introduces the GameController class to bridge game logic with the infrastructure layer. It facilitates operations like creating games, handling attacks, and setting the GUI controller.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/adapters/GameController.java
@@ -0,0 +1,48 @@
+package es.upm.etsisi.fis.fisfleet.infrastructure.adapters;
+
+import es.upm.etsisi.fis.controller.ControladorPartida;
+import es.upm.etsisi.fis.model.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameController {
+    private final ControladorPartida controladorPartida;
+
+    public GameController(WebGameViewController vistaWeb) {
+        this.controladorPartida = ControladorPartida.getInstance(null);
+        this.controladorPartida.setGui(vistaWeb);
+    }
+
+    public void realizaContrataque(Partida partidaActual) {
+        this.controladorPartida.realizaContrataque(partidaActual);
+    }
+
+    public void crearPartida(IJugador usuariologued, IJugador maquina, IPuntuacion puntuacion, IMovimiento movimiento) {
+        this.controladorPartida.crearPartida(usuariologued, maquina, puntuacion, movimiento);
+    }
+
+    public void crearPartida(IJugador usuariologued, IJugador maquina, IPuntuacion puntuacion, IMovimiento movimiento, boolean espera) {
+        this.controladorPartida.crearPartida(usuariologued, maquina, puntuacion, movimiento, espera);
+    }
+
+    public void crearPartida(IJugador usuariologued, IPuntuacion puntuacion, IMovimiento movimiento) {
+        this.controladorPartida.crearPartida(usuariologued, puntuacion, movimiento);
+    }
+
+    public void lanzaArtilleria(Partida partidaActual, IAcorazado acorazado) {
+        this.controladorPartida.lanzaArtilleria(partidaActual, acorazado);
+    }
+
+    public void reparase(Partida partidaActual, ISubmarino submarino) {
+        this.controladorPartida.reparase(partidaActual, submarino);
+    }
+
+    public void mostrar(Partida partidaActual, IPatrullero patrullero) {
+        this.controladorPartida.mostrar(partidaActual, patrullero);
+    }
+
+    public void setGui(WebGameViewController gui) {
+        this.controladorPartida.setGui(gui);
+    }
+
+}
