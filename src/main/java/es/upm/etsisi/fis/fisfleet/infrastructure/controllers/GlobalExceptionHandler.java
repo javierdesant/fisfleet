46936f933a536de21ef699d244ca9547228e22ba
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-21 03:20:32 +0200

    feat: add handler for SecurityException
    Introduce a new exception handler in GlobalExceptionHandler to manage SecurityException. Returns a 403 Forbidden response with the exception message.

--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/GlobalExceptionHandler.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/controllers/GlobalExceptionHandler.java
@@ -36,4 +36,9 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
+
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<String> handleSecurityException(SecurityException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ex.getMessage());
+    }
 }
