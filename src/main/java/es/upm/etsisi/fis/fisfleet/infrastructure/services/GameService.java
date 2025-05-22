b03b6def4a94530ddeff4060baac316f5c55ee01
Javier de Santiago <j.desantiagosoto@gmail.com>
2025-05-22 18:29:55 +0200

    feat: add new game actions and helper methods in GameService
    Introduced methods for additional game actions like counterattack, artillery attack, and submarine repair. Also added utility methods to retrieve game state, validate moves, and check game over status. These changes enhance functionality and support finer control in game mechanics.

--- src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/GameService.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/GameService.java
@@ -9,11 +9,27 @@ public interface GameService {  // TODO: fix GameViewDTO vs GameStateDTO
 
     GameViewDTO createPvEMatch(TDificultad difficulty);
 
+    GameStateDTO getGameById(Long gameId);
+
+    GameViewDTO getGameViewForPlayer(Long gameId, Long playerId);
+
     GameStateDTO performAttack(MoveRequest request);
 
-    GameStateDTO performSpecialAttack(MoveRequest request);
+    GameStateDTO performCounterAttack(MoveRequest request);
+
+    GameStateDTO launchArtilleryAttack(MoveRequest request);
+
+
+    // TODO: turnOfPlayer vs playerId?
+    GameStateDTO repairSubmarine(Long gameId, Long playerId);
+
+    GameStateDTO revealRow(Long gameId, Long playerId, int y);
 
     boolean canPlayerPerformAction(Long gameId, Long playerId);
+
+    boolean isGameOver(Long gameId);
+
+    boolean validateMove(MoveRequest request);
 }
 
 //    GameStateDTO createPvPMatch();
