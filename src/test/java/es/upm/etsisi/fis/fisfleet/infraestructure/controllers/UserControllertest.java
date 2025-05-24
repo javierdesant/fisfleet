1f1b6029f62183698afb60293b51165d80c4a4c6
Raul Gonzalo Toapanta <gonzalo.tpaucar@alumnos.upm.es>
2025-05-24 22:31:15 +0200

    Rearrange files for better visualization

---
+++ src/test/java/es/upm/etsisi/fis/fisfleet/infraestructure/controllers/UserControllertest.java
@@ -0,0 +1,4 @@
+package es.upm.etsisi.fis.fisfleet.infraestructure.controllers;
+
+public class UserControllertest {
+}
---
+++ src/test/java/es/upm/etsisi/fis/fisfleet/infraestructure/services/UserServiceImplTest.java
@@ -0,0 +1,4 @@
+package es.upm.etsisi.fis.fisfleet.infraestructure.services;
+
+public class UserServiceImplTest {
+}
