696c44f3f8b3a520f75bb3918e8f5dc89bafad3e
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-26 01:56:42 +0200

    feat: add GameResultService with necessary basic CRUD methods
    Introduces a new service class to manage game results, including a
    method to persist finished games and placeholders for read and delete
    operations.

---
+++ src/main/java/es/upm/etsisi/fis/fisfleet/infrastructure/services/GameResultService.java
@@ -0,0 +1,38 @@
+package es.upm.etsisi.fis.fisfleet.infrastructure.services;
+
+import es.upm.etsisi.fis.fisfleet.api.dto.responses.GameResultResponse;
+import es.upm.etsisi.fis.fisfleet.domain.entities.GameResultEntity;
+import es.upm.etsisi.fis.fisfleet.domain.repositories.GameResultRepository;
+import es.upm.etsisi.fis.fisfleet.domain.repositories.PlayerRepository;
+import es.upm.etsisi.fis.model.Partida;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GameResultService {
+    private final GameResultRepository repo;
+
+    @Transactional
+    public void persistFinished(Partida partida) {
+        assert partida.fin();
+
+        // TODO: Partida to GameResult mapper
+        //  Long winner = Long.valueOf(partida.getTurnoName());
+        GameResultEntity ent = new GameResultEntity();
+
+        repo.save(ent);
+    }
+
+    // TODO
+    public GameResultEntity read(Long gameId) {
+        return null;
+    }
+
+    public void delete(Long gameId) {
+
+    }
+}
