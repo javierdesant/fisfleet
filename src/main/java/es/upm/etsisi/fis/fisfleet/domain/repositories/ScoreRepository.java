bc48b07d539672e69a83a5d27b18194b9beeb147
JavierDeSant <j.desantiagosoto@gmail.com>
2025-05-26 21:56:35 +0200

    feat: add repository methods for game results and scores
    Introduces new methods in repositories to fetch specific data:
    - Retrieves the top 10 scores for a player, ordered by score.
    - Fetches all game results by game ID, ordered by start date.
    - Lists all scores, ordered by points in descending order.

--- src/main/java/es/upm/etsisi/fis/fisfleet/domain/repositories/ScoreRepository.java
+++ src/main/java/es/upm/etsisi/fis/fisfleet/domain/repositories/ScoreRepository.java
@@ -3,5 +3,12 @@ package es.upm.etsisi.fis.fisfleet.domain.repositories;
 import es.upm.etsisi.fis.fisfleet.domain.entities.ScoreEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 public interface ScoreRepository extends JpaRepository<ScoreEntity, Long> {
+    /**
+     * Todas las puntuaciones, ordenadas por puntos descendentes
+     */
+    List<ScoreEntity> findAllByOrderByPointsDesc();
 }
